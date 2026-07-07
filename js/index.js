dce3bc9d397e8b7187496fcd74ca7e0713ccb975
Sergey <sergtspdevkz@gmail.com>
2026-07-07 20:59:34 +0500

    index

--- js/index.js
+++ js/index.js
@@ -9,3 +9,34 @@ likeButtons.forEach((button) => {
 const yearElement = document.querySelector('.footer__year');
 const currentYear = new Date().getFullYear();
 yearElement.textContent = currentYear;
+
+const popup = document.querySelector('.popup');
+const editButton = document.querySelector('.profile__edit-button');
+const closeButton = document.querySelector('.popup__close');
+const formElement = document.querySelector('.popup__form');
+
+const nameInput = document.querySelector('.popup__input_type_name');
+const jobInput = document.querySelector('.popup__input_type_about');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__description');
+
+function openPopup() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+  popup.classList.add('popup_opened');
+}
+
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup();
+}
+
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', handleFormSubmit);
